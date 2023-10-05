30e8f3b7c52715a3ac2593f185c9dd3d4248fe27
Ramón Schlosser <schlosser@itplusx.de>
2023-10-05 16:54:23 +0200

    chore: remove deprecated clearCacheOnLoad key

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,6 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'ITplusX',
     'author_email' => 'mail@itplusx.de',
     'author_company' => 'ITplusX GmbH',
-    'clearCacheOnLoad' => 1,
     'constraints' => [
         'depends' => [
             'php' => '8.1.0-8.2.99',
