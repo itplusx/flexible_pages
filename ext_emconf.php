c01697b290a20a6ad8ab37aee9f550bba1f76753
Ramón <schlosser@itplusx.de>
2024-01-04 14:55:01 +0000

    chore(deps): raise PHP version constraint to 8.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'ITplusX GmbH',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.2.99',
+            'php' => '8.1.0-8.3.99',
             'typo3' => '12.4.0-12.4.99',
             'fluid_styled_content' => '12.4.0-12.4.99'
         ],
